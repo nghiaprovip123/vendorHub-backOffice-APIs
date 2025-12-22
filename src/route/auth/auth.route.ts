8a306bba85ae0eefc87605d6eee188ecea0da590
nghiaprovip123 <wrefree549@gmail.com>
2025-12-22 18:55:09 +0700

    Google Open Authentication Feature

--- src/route/auth/auth.route.ts
+++ src/route/auth/auth.route.ts
@@ -4,7 +4,9 @@ import { registerController } from '@/controller/auth/register.controller';
 import { loginController } from '@/controller/auth/login.controller';
 import { refreshTokenController } from '@/controller/auth/refreshToken.controller'
 import { logoutController } from '@/controller/auth/logout.controller'
+import { googleRedirectController } from '@/controller/auth/googleRedirect.controller'
 import { validate } from '@/middlewares/validate.middleware';
+import { googleAuthController } from '@/controller/auth/callback/google/googleAuth.controller'
 const router = Router();
 
 // ============================================
@@ -36,9 +38,25 @@ router.post(
 // ============================================
 // auth/logout
 // ============================================
-router.get(
+router.post(
     "/logout",
     logoutController
+);
+
+// ============================================
+// auth/google
+// ============================================
+router.get(
+    "/google",
+    googleRedirectController
+)
+
+// ============================================
+// auth/callback/google
+// ============================================
+router.get(
+    "/callback/google",
+    googleAuthController
 )
 
 export default router;
