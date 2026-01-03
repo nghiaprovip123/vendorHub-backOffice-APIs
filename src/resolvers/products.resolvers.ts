83ae93ab0468ced30c6f128a4051b731332f5310
nghiaprovip123 <wrefree549@gmail.com>
2026-01-03 14:55:56 +0700

    Test CICD

--- src/resolvers/products.resolvers.ts
+++ src/resolvers/products.resolvers.ts
@@ -3,7 +3,8 @@ import { createProductsService } from "@/services/products/createProducts.servic
 import { PricingType } from "@prisma/client";
 import { createProductsValid } from "@/validation/products/products.validation";
 
-export const productResolver = {
+export const productResolver = 
+{
   Mutation: {
     createProducts: async (_: unknown, args: { input: any }, ctx: any) => {
       try {
@@ -23,3 +24,4 @@ export const productResolver = {
     },
   },
 };
+
