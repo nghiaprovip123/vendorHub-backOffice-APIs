bf6c4eb97bc711a4389a470b27d43bc08d47a34c
nghiaprovip123 <wrefree549@gmail.com>
2026-01-07 14:20:43 +0700

    Enhance the Organization of Update Staff Logic by layering into 2 layers: Service+Validation

--- src/resolvers/staffs/update-staff.resolver.ts
+++ src/resolvers/staffs/update-staff.resolver.ts
@@ -1,61 +1,13 @@
-import { prisma } from "@/lib/prisma"
-import { replaceImageOnCloudinary } from "@/utils/replace-img-helper"
 import { GraphQLUpload } from "graphql-upload-minimal";
+import { updateStaffService } from "@/services/staffs/update-staff.service"
 const updateStaff = async (
     _: unknown,
     args: { input: any },
     ctx: any
 ) => {
     try {
-        let avatar_url: string | undefined
-
-        if (args.input.avatar) {
-            const file = await args.input.avatar;
-            const stream = file.createReadStream()
-            const replaceImage = await replaceImageOnCloudinary(stream, "Staff Avatar Storage", "rqvosutgecru7tutqcdu", true)
-            avatar_url = replaceImage.secure_url
-        }
-    
-        const result = await prisma.$transaction( async(tx) => {
-            const staff = await tx.staff.update(
-                {
-                    where: {
-                        id: args.input.id
-                    },
-                    data: {
-                        fullName: args.input.fullName,
-                        avatar_url: avatar_url,
-                        timezone: args.input.timezone,
-                        isActive: args.input.isActive,
-                        isDeleted: args.input.isDeleted,
-                    }
-                }
-            )
-        
-            await tx.workingHour.deleteMany({
-                where: { staffId: staff.id },
-              });
-              
-              const workingHours = await Promise.all(
-                args.input.workingHours.map((wh: any) =>
-                  tx.workingHour.create({
-                    data: {
-                      staffId: staff.id,
-                      day: wh.day,
-                      startTime: wh.startTime,
-                      endTime: wh.endTime,
-                    },
-                  })
-                )
-              );
-            return {
-                ...staff,
-                workingHours: workingHours
-            }
-        } 
-    )
-    
-    return result
+        const updateStaff = updateStaffService(args.input)
+        return updateStaff
     } catch (error: any) {
         throw new Error("Unknown Error")
     }
