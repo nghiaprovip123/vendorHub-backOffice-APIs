d30c537b2e0f163868a4608362300e4d8b53b12b
nghiaprovip123 <wrefree549@gmail.com>
2026-01-13 17:16:09 +0700

    Layerize SendOTP Logic

--- src/controllers/send-otp.controller.ts
+++ src/controllers/send-otp.controller.ts
@@ -3,6 +3,7 @@ import sql from "@/lib/postgresql"
 import ApiError from "@/utils/ApiError"
 import crypto from "crypto"
 import { sendOtpEmailRegisteration } from "@/lib/send-otp-helper"
+import { SendOTPService } from "@/services/auth/send-otp.service"
 type SendOTPControllerType = {
     phone: string,
     type: string
@@ -11,55 +12,12 @@ export const SendOTPController = async (req: Request, res: Response, next: NextF
     try {
         const { phone, type, email } = req.body
   
-        if (!email || !phone) {
-          throw new ApiError(400, 'Phone is required')
-        }
-      
-        const [{ count }] = await sql`
-          SELECT COUNT(*)::int AS count
-          FROM otps
-          WHERE type = ${type}
-            AND email = ${email}
-            AND phone = ${phone}
-            AND createdat > NOW() - INTERVAL '15 minutes'
-        `
-      
-        if (count >= 3) {
-          throw new ApiError(429, 'OTP rate limit exceeded')
-        }
-      
-        await sql`
-          UPDATE otps
-          SET isverified = true,
-              isactive = false
-          WHERE type = ${type}
-            AND email = ${email}
-            AND phone = ${phone}
-            AND isverified = false
-            AND isactive = true
-        `
-      
-        const otp = crypto.randomInt(100000, 999999).toString()
-        // const otpHash = crypto.createHash('sha256').update(otp).digest('hex')
-      
-        const [{ expiresat }] = await sql`
-          INSERT INTO otps (type, email, otp, expiresat, phone)
-          VALUES (${type}, ${email}, ${otp}, NOW() + INTERVAL '5 minutes', ${phone})
-          RETURNING expiresat
-        `
-        const sendOTP = await sql `
-            SELECT * FROM otps
-            WHERE email = ${email}
-                AND isactive = 'true'
-                AND isverified = 'false'
-        `
-        await sendOtpEmailRegisteration(email, otp);
-
-        res.json({
+        const result = await SendOTPService({phone, type, email})
+        return res.json({
           success: true,
-          otp: sendOTP,
-          expiresAt: expiresat,
-          remainingAttempts: 3 - count
+          otp: result.otp,
+          expiresAt: result.expiresAt,
+          remaining: result.remainingAttempts
         })
     }
     catch(err){
---
+++ src/services/auth/send-otp.service.ts
@@ -0,0 +1,74 @@
+import { Request, Response, NextFunction } from "express"
+import sql from "@/lib/postgresql"
+import ApiError from "@/utils/ApiError"
+import crypto from "crypto"
+import { sendOtpEmailRegisteration } from "@/lib/send-otp-helper"
+
+type SendOTPServiceType = {
+    phone: string,
+    type: string,
+    email: string,
+}
+export const SendOTPService = async ({phone, type, email}: SendOTPServiceType) => {
+    if(!email || !phone) {
+        throw new ApiError(400, 'Email is required')
+    }
+
+    const [{count}] = await sql`
+        SELECT COUNT(*)::int AS count 
+        FROM otps
+        WHERE type = ${type}
+            AND email = ${email}
+            AND createdat > NOW() - INTERVAL '15 minutes'
+    `
+    if (count > 3) {
+        throw new ApiError(429, "KHÔNG THỂ GỬI OTP VÌ SỐ LẦN GỬI OTP ĐÃ ĐẠT GIỚI HẠN")
+    }
+
+    await sql`
+        UPDATE otps
+        SET isverified = true
+            AND isactive = false
+        WHERE type = ${type}
+            AND email = ${email}
+            AND phone = ${phone}
+            AND isverified = false
+            AND isactive = true
+    `
+
+    const generateOTP = await crypto.randomInt(100000, 999999).toString()
+
+    const [{expiresat}] = await sql`
+        INSERT INTO otps (
+            type, 
+            email,
+            otp,
+            expiresat,
+            phone
+        ) VALUES (
+            ${type},
+            ${email},
+            ${generateOTP},
+            NOW() + INTERVAL '15 Minutes',
+            ${phone}
+        )
+        RETURNING expiresAt
+    `
+
+    const [sendOTP] = await sql`
+        SELECT *
+        FROM otps
+        WHERE email = ${email}
+        AND isactive = true
+        AND isverified = false
+    `
+
+
+    await sendOtpEmailRegisteration(email, generateOTP);
+    return {
+        otp: sendOTP,
+        expiresAt: expiresat,
+        remainingAttempts: 3 - count
+    }
+}
+  
